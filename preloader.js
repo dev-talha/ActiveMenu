2fc0474c62748604bac63396387903587c75e41d
Abu Talha <53362081+dev-talha@users.noreply.github.com>
2020-10-20 18:38:39 +0600

    Create preloader.js

---
+++ preloader.js
@@ -0,0 +1,13 @@
+    <div class="loader">
+           <img class="animate animate__animated animate__infinite animate__pulse flip infinite" src="/Theme/img/logo.png"/>
+    </div>
+    
+<div class="loader">
+           <img class="animate animate__animated animate__infinite animate__pulse flip infinite" src="/Theme/img/logo.png"/>
+    </div>
+  
+  jQuery(function ($) {
+ jQuery(window).on('load',function(){
+		jQuery('.loader').fadeOut(500);
+	});
+  )};
